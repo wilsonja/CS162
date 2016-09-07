941f40f7ce0c222cd64daa1c3ef02864526dfdc0
Jacob Wilson <wilsonja117@gmail.com>
2016-09-07 10:23:00 -0700

    Create main.cpp

---
+++ Character-Tournament/main.cpp
@@ -0,0 +1,33 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * Program Filename: assignment4
+ * * Author: Jacob Wilson
+ * * Date: 2/28/14
+ * * Description: This program is a modification of Assignment 3. A
+ * *  user is now able to enter a team size which can then be
+ * *  populated with multiple team members. Each team member is also
+ * *  now able to be given a unique name. Players rotate throught their
+ * *  lineup as they battle. A stack is created which holds the
+ * *  players as they lose.
+ * * Input: int from user serves as team size
+ * * Output: display lineups, battles, and results
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Game.hpp"
+
+int main()
+{
+    srand((unsigned int) time(NULL));
+    int members;
+    
+    /* obtain number of players from user */
+    std::cout << "How many members for each team?" << std::endl;
+    std::cin >> members;
+    
+    /* create game object passing number of players */
+    Game game(members);
+    
+    return 0;
+}
