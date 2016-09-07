99fcfe2f4a8db95fe342c8545a6888d76ca79e68
Jacob Wilson <wilsonja117@gmail.com>
2016-09-07 10:16:15 -0700

    Create menu.hpp

---
+++ Character-Battle-Game/menu.hpp
@@ -0,0 +1,20 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * Program Filename: Assignment3
+ * * Author: Jacob Wilson
+ * * Date: 2/14/16
+ * * Description: This program simulates a dice battle between two
+ * *  characters. Each character has different attack, defense, armor,
+ * *  and strength attributes. Some characters also have special
+ * *  traits that are called at specific points.
+ * * Input: choice of two characters
+ * * Output: attack and defense values for each round
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef MENU_HPP
+#define MENU_HPP
+
+/* prototypes for menu-related functions */
+void displayMenu();
+int getChoice();
+
+#endif
